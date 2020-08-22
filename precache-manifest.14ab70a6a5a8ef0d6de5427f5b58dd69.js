67622b99cc482d37e1f27d62bdf562d3bd05267a
Jonathan Dunlap <jonnyd2885@gmail.com>
2020-08-22 21:15:36 +0700

    first commit

---
+++ precache-manifest.14ab70a6a5a8ef0d6de5427f5b58dd69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94432572fbbf3255779ff99e88e6e87b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50eb753d68d7fe9cae68",
+    "url": "/static/css/main.9447384d.chunk.css"
+  },
+  {
+    "revision": "9d2ae3c2628a4c5fd0d4",
+    "url": "/static/js/2.67691ccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.67691ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50eb753d68d7fe9cae68",
+    "url": "/static/js/main.4cdbec63.chunk.js"
+  },
+  {
+    "revision": "31de383a84f0b531942d",
+    "url": "/static/js/runtime-main.408e7f6b.js"
+  }
+]);
